e5797c2e26aca896d17360e0f378ae5160619119
Meredith Reece <mardebugger@gmail.com>
2018-08-09 22:19:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  def index
+    render :index
+  end
+  
   def current_user
     session[:name] ||= nil
   end
