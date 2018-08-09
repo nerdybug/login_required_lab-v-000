be26e9329098ea9b83f6f772e86044a24ecab19f
Meredith Reece <mardebugger@gmail.com>
2018-08-09 22:45:17 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
   def create(*args)
     if params[:name] && params[:name] != ""
       session[:name] = params[:name]
+      render 'secrets/show'
     else
       redirect_to controller: 'sessions', action: 'new'
     end
