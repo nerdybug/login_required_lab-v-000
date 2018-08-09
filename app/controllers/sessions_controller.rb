ac3273541f1ec1b0aff1a8586e18a57c97ce7953
Meredith Reece <mardebugger@gmail.com>
2018-08-09 22:05:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,17 @@ class SessionsController < ApplicationController
   end
 
   def create(*args)
-    if session[:name] == nil || session[:name] == ""
-      binding.pry
-      redirect_to '/new'
-    elsif params[:name]
+
+    if params[:name] && params[:name] != ""
       session[:name] = params[:name]
+    else
+      redirect_to '/new'
     end
+    # if session[:name] == nil || session[:name] == ""
+    #   binding.pry
+    #   redirect_to '/new'
+    # elsif params[:name]
+    #   session[:name] = params[:name]
+    # end
   end
 end
